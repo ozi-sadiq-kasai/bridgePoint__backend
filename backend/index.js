40a7e9578703459a694c0aaf3794d507f50860aa
ozi <ozisadiqkasai@gmail.com>
2025-05-28 18:36:42 +0100

    uploads file routing

--- backend/index.js
+++ backend/index.js
@@ -17,6 +17,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 app.use(cors({
   origin: function (origin, callback) {
     if (!origin || allowedOrigins.includes(origin)) {
