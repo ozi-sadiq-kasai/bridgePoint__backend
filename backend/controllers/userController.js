4882eac6cbb6ed894cefe6002b3d4e6ac2b3417d
ozi <ozisadiqkasai@gmail.com>
2025-05-28 19:21:32 +0100

    file pathing

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,7 +5,7 @@ export const createUserDispute = async (req, res) => {
     try {
         const data = {
             ...req.body,
-            file: req.file ? req.file.path : null, // Handle file upload if present          
+            file: req.file ? req.file.filename: null, // Handle file upload if present          
         }
         const userDispute = await User.create(data);
         res.status(201).json({ message: 'User dispute submitted successfully', userDispute });
