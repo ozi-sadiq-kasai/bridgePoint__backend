93ce2a3a55e7c6def3d1f468a9246dd6ddc41900
ozi <ozisadiqkasai@gmail.com>
2025-05-30 21:22:59 +0100

    remove file upload

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,8 @@
 import express from 'express';
 import { createUserDispute } from '../controllers/userController.js';
-import upload from '../middleware/uploadMiddleware.js';
 
 const router = express.Router();
 
-
-router.post('/', upload.single('file'), createUserDispute);
-
-
+router.post('/', createUserDispute);
 
 export default router;
